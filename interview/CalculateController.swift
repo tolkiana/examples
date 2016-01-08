18be92a235da39b107d7e92b595bc69bc5e061c6
Nelida Velazquez <tolkiana@gmail.com>
2016-01-07 19:55:24 -0500

    Created basic layout

---
+++ interview/CalculateController.swift
@@ -0,0 +1,13 @@
+//
+//  CalculateController.swift
+//  interview
+//
+//  Created by Nelida Velazquez on 1/7/16.
+//  Copyright © 2016 Tolkiana. All rights reserved.
+//
+
+import Cocoa
+
+class CalculateController: NSObject {
+
+}
---
+++ interview/OptionsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  OptionsViewController.swift
+//  interview
+//
+//  Created by Nelida Velazquez on 1/7/16.
+//  Copyright © 2016 Tolkiana. All rights reserved.
+//
+
+import UIKit
+
+class OptionsViewController: UIViewController {
+
+}
